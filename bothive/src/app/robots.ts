92c730a39ddef98020ab028df8502bab5a224f8f
Jeremy <akinlorinjeremiah@gmail.com>
2025-12-20 08:48:57 +0100

    Implement robot.txt fix again

--- bothive/src/app/robots.ts
+++ bothive/src/app/robots.ts
@@ -2,13 +2,11 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: [
-            {
-                userAgent: '*',
-                allow: '/',
-                disallow: ['/api/', '/_next/'],
-            }
-        ],
+        rules: {
+            userAgent: '*',
+            allow: '/',
+            disallow: ['/api/', '/dashboard/', '/checkout/'],
+        },
         sitemap: 'https://bothive.cloud/sitemap.xml',
     };
 }
